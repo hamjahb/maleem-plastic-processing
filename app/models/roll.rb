1c436065d3f73dffab9094aa59d3aef3ea0fb136
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-10-08 16:29:27 +0300

    change callback to film

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,13 +1,13 @@
 class Roll < ApplicationRecord
   belongs_to :order
   
-  after_commit :update_order_printed
+  after_commit :update_order_film
 
 
 
   private
   # updates order printed weight after commiting a new roll
-  def update_order_printed
+  def update_order_film
     sum_of_order_rolls = Roll.where(order_id: order_id).sum(:roll_weight)
     order.update(total_film_made: sum_of_order_rolls)
   end
