51d2ee8a962e5b02c9ef13dfa6c18d4f5758a9a4
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-10-13 15:30:59 +0300

    total printed updates order

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,7 +1,7 @@
 class Roll < ApplicationRecord
   belongs_to :order
   
-  after_commit :update_order_film
+  after_commit :update_order_film, :update_total_printed
 
 
 
@@ -10,5 +10,10 @@ class Roll < ApplicationRecord
   def update_order_film
     sum_of_order_rolls = Roll.where(order_id: order_id).sum(:roll_weight)
     order.update(total_film_made: sum_of_order_rolls)
+  end
+
+  def update_total_printed
+    sum_of_of_printed_rolls = Roll.where(order_id: order_id).sum(:printed_weight)
+    order.update(total_printed_weight: sum_of_of_printed_rolls)
   end
 end
