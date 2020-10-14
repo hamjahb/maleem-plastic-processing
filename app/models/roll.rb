5d1ec4613168a3216c0cf1173225574e525716c4
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-10-14 13:54:06 +0300

    add update order when printing complete

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,7 +1,7 @@
 class Roll < ApplicationRecord
   belongs_to :order
   
-  after_commit :update_order_film, :update_total_printed
+  after_commit :update_order_film, :update_total_printed, :update_order_done_printing
 
 
 
@@ -12,8 +12,18 @@ class Roll < ApplicationRecord
     order.update(total_film_made: sum_of_order_rolls)
   end
 
+  # updates the total printed for an order when a roll is finished printing
   def update_total_printed
     sum_of_of_printed_rolls = Roll.where(order_id: order_id).sum(:printed_weight)
     order.update(total_printed_weight: sum_of_of_printed_rolls)
   end
+
+  # if there are no more rolls to be printed in an order complete printing
+  def update_order_done_printing
+    all_order_rolls = Roll.where(order_id: order_id)
+    if all_order_rolls.where('roll_print_complete = ?', false).count == 0
+      order.update(print_complete: true)
+    end
+  end
+  
 end
