665d76c1b26a8c4c7c98a9af062f24647a72041f
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-11-09 11:34:27 +0300

    deliveries filter working

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,8 +1,32 @@
 class DeliveriesController < ApplicationController
   def index
-    @orders = Order.where(("delivered= false") && ("cut_complete= true")).order(:id)
+    @orders = Order.where(cut_complete: true)
+    @orders = @orders.where(delivered: false).order(:id)
   end
 
   def edit
+    @order = Order.find(params[:id])
   end
+
+  def update
+    respond_to do |format|
+      @order = Order.find(params[:id])
+      if @order.update(order_params)
+        format.html { redirect_to deliveries_path, notice: 'Order was successfully Delivered.' }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:delivered)
+  end
+
 end
