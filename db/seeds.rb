4e2924c1508da44ce463290855bf4ae204293ada
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-11-09 14:10:24 +0300

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,10 @@ Order.create(customer_name: "Center Point", film_width: 45, height: 45, material
 Order.create(customer_name: "Sun and Sands", film_width: 25, height: 50, material: "LD", specifications: "punch shopping bag", size: "S", order_weight_kg: 200000, thickness: 40, packing_weight: 10 )
 
 
+Order.create(customer_name: "Dunkin", film_width: 60, height: 35, material: "HD", specifications: "punch bottom gusset", size: "M", order_weight_kg: 2000, thickness: 60, packing_weight: 15, total_film_made: 2200, print_complete: false, total_printed_weight: 2100, packing_weight: 15)
+
+Order.create(customer_name: "Print", film_width: 45, height: 45, material: "LD", specifications: "punch bottom gusset", size: "M", order_weight_kg: 500, thickness: 60, packing_weight: 15, total_film_made: 540, print_complete: true, total_printed_weight: 530, packing_weight: 15)
+
 Roll.create(order_id: 1, machine_number: 6, roll_weight: 400, operator_name: "mojo jo jo" )
 
 
