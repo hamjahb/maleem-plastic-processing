19f02371761b8a974d7914b013fd7273f747ee11
Hisham Aljahbli <h.aljahbli@gmail.com>
2020-11-17 14:53:39 +0300

    add QA sample weight to rolls

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,21 +18,20 @@ Order.create(customer_name: "Dunkin", film_width: 60, height: 35, material: "HD"
 
 Order.create(customer_name: "Print", film_width: 45, height: 45, material: "LD", specifications: "punch bottom gusset", size: "M", order_weight_kg: 500, thickness: 60, packing_weight: 15, total_film_made: 540, print_complete: true, total_printed_weight: 530, packing_weight: 15)
 
-Roll.create(order_id: 1, machine_number: 6, roll_weight: 400, operator_name: "mojo jo jo" )
+
+Roll.create(order_id: 1, machine_number: 6, roll_weight: 400, operator_name: "mojo jo jo", qa_sample_weight: 40 )
 
 
-
-Roll.create(order_id: 2, machine_number: 6, roll_weight: 234, operator_name: "HIM" )
-
+Roll.create(order_id: 2, machine_number: 6, roll_weight: 234, operator_name: "HIM", qa_sample_weight: 46 )
 
 
-Roll.create(order_id: 1, machine_number: 2, roll_weight: 350, operator_name: "Fuzzy Lumpkins" )
+Roll.create(order_id: 1, machine_number: 2, roll_weight: 350, operator_name: "Fuzzy Lumpkins", qa_sample_weight: 46 )
 
 
+Roll.create(order_id: 1, machine_number: 3, roll_weight: 274, operator_name: "Gang Green Gang", roll_print_complete: true, printed_weight:252, qa_sample_weight: 23 )
 
-Roll.create(order_id: 1, machine_number: 3, roll_weight: 274, operator_name: "Gang Green Gang", roll_print_complete: true, printed_weight:252 )
 
-Roll.create(order_id: 3, machine_number: 2, roll_weight: 560, operator_name: "Gang Green Gang", roll_print_complete: true, printed_weight:540 )
+Roll.create(order_id: 3, machine_number: 2, roll_weight: 560, operator_name: "Gang Green Gang", roll_print_complete: true, printed_weight:540, qa_sample_weight: 34 )
 
 
 
