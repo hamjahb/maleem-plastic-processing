906c5e4a08bb81fddd15dced236fc0960d18b394
Hisham Aljahbli <h.aljahbli@gmail.com>
2021-03-03 21:40:02 +0300

    add update delivered quantity to order

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,13 @@
 class Delivery < ApplicationRecord
   belongs_to :order
+
+  after_commit :update_order_delivered_quantity
+
+  private
+  # updates order printed weight after commiting a new roll
+  def update_order_delivered_quantity
+    sum_of_delivered_quantity = Delivery.where(order_id: order_id).sum(:quantity)
+    order.update(delivered_quantity: sum_of_delivered_quantity)
+  end
+
 end
