c4aa2838c9f353289fdcb270a9b0737181382572
Hisham Aljahbli <h.aljahbli@gmail.com>
2021-03-23 22:46:10 +0300

    change films view to filmtoproduce comparision

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,5 +1,5 @@
 class FilmsController < ApplicationController
   def index
-    @orders = Order.where("total_film_made < order_weight_kg").order(:id)
+    @orders = Order.where("total_film_made < film_to_produce").order(:id)
   end
 end
